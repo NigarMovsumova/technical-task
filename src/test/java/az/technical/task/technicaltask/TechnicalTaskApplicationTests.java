e96cf1efb11de78f7ed7b1f8624778a766f0474b
Nigar <nigar.movsumova@pashabank.az>
2019-11-01 21:36:43 +0400

    Initial commit

---
+++ src/test/java/az/technical/task/technicaltask/TechnicalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package az.technical.task.technicaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
